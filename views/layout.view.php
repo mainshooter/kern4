8d47d21bb98e9a89bcc07ecd0ef7faf867b72da8
Peter 4A5A <498883@edu.rocmn.nl>
2018-03-21 09:46:25 +0100

    Fixed issue 4

--- views/layout.view.php
+++ views/layout.view.php
@@ -4,12 +4,13 @@
 <head>
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="resources/css/global.css">
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>	
-    <link href='https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two' rel='stylesheet' type='text/css'>	
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two' rel='stylesheet' type='text/css'>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
+		<img class="logo" src="resources/img/logo.png">
     <?php require $content; ?>
 </body>
 </html>
